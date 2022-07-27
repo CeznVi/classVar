4b27c972d8d45f7523a5a765b373afe0b07578c7
Viktor <Cezn.Vi@gmail.com>
2022-07-27 20:19:50 +0300

    final version

--- Source.cpp
+++ Source.cpp
@@ -8,24 +8,40 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	var a = 2;
-	var b = 2;
-	var c = 3.045;
-	var d;
-
-	std::cout << "a =";
+	var a = 15;
+	var b = "Hello";
+	var c = 7.8;
+	var d = "50";
+	std::cout << "a = ";
 	a.print();
-	std::cout << "b =";
+	std::cout << "b = ";
 	b.print();
-	std::cout << "c =";
+	std::cout << "c = ";
 	c.print();
+	std::cout << "b = a + d = ";
+	b = a + d;
+	b.print();
+	std::cout << "a == b == ";
+		if(a == b)
+			std::cout << "Дорівнює\n";
+		else 
+			std::cout << "Недорівнює\n";
+	std::cout << "a += d = ";
+	a += d;
+	a.print();
+	var e = "Microsoft";
+	var f = "Windows";
+	var g;
+	std::cout << "e = ";
+	e.print();
+	std::cout << "f = ";
+	f.print();
+	std::cout << "g = e * f = ";
+	g = e * f;
+	g.print();
+	std::cout << "g = e / f = ";
+	g = e / f;
+	g.print();
 
-	//d = a / c;
-	std::cout << "d =";
-	d.print();
-	if(a == b)
-		std::cout << "a ==b";
-	else 
-		std::cout << "FALSE   a <b";
 	return 0;
 }
